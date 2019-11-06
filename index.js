a0d4a1a7d071666b20d98a3a7b1a28ec3d8efead
Manuel Montanez <mhanuel@gmail.com>
2019-11-06 16:33:02 -0500

    Se agrega cors para habilitar consumo externo

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 var mysql = require('mysql');
+var cors = require('cors')
 var dashboardRouter = require('./routes/dashboard');
 var bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/dashboard', dashboardRouter);
 app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
