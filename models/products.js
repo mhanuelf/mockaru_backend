29c8973c3a8bf2778a16aff3687bffba0ffc092f
Manuel Montanez <mhanuel@gmail.com>
2019-11-06 17:34:16 -0500

    Se ajsuta la consulta de productos

--- models/products.js
+++ models/products.js
@@ -8,8 +8,7 @@ var Product = function (productos) {
 
 Product.getProductById = function (productId, result) {
     console.log("Consultando productos por id" + productId);
-    var sqlQuery = "select TIPO_RADICACION, TIPO_PRODUCTO, count(1) as cantidad from INFO_PQR where " +
-        "TIPO_PRODUCTO = '" + productId +"' group by TIPO_RADICACION, TIPO_PRODUCTO";
+     var sqlQuery = "select p.TIPO_PRODUCTO,c.DESCRIPCION,TIPO_RADICACION, count(1) as CANTIDAD from INFO_PQR p , INFO_CATALOGO c where p.TIPO_PRODUCTO = c.TIPO_PRODUCTO and p.TIPO_PRODUCTO = '"+productId+"' group by TIPO_RADICACION, TIPO_PRODUCTO, c.DESCRIPCION";
     sql.query(sqlQuery, function (err, res) {
         if (err) {
             console.log("error: ", err);
