c274a41195381672a00572fce182899f02bfe795
Manuel Montanez <mhanuel@gmail.com>
2019-11-06 17:26:29 -0500

    Se agrega informacion a las consultas de productos

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,9 +3,9 @@ var Product = require('../models/products');
 
 
 // Display detail data for a specific Product.
-exports.product_detail = function(productId, res) {
-    Product.getProductById(function(err, product) {
-        console.log('controller product by id'+productId);
+exports.product_detail = function(req, res) {
+    Product.getProductById(req.params.productId,function(err, product) {
+        console.log('controller product by id'+req.params.productId);
         if (err)
           res.send(err);
           console.log('res', product);
