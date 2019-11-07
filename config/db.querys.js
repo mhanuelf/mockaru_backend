d903173924e0844b03ee7765627546af99689885
Manuel Montanez <mhanuel@gmail.com>
2019-11-07 12:05:39 -0500

    Se agrega tipo de documento por peticion de moscarella

--- config/db.querys.js
+++ config/db.querys.js
@@ -5,6 +5,6 @@ module.exports = {
     PQR_LIST_PQR_PER_PRODUCT_TYPE: "select  c.DESCRIPCION AS PRODUCTO,count(p.TIPO_PRODUCTO) as CANTIDAD, c.TIPO_PRODUCTO  from INFO_PQR p , INFO_CATALOGO c where p.TIPO_PRODUCTO = c.TIPO_PRODUCTO group by p.TIPO_PRODUCTO,  c.DESCRIPCION",
     GET_PQR_BY_PRODUCT_TYPE: "select TIPO_RADICACION as tipoRadicacion, DESCRIPCION_CASO as descripcionCaso, count(TIPO_RADICACION) as cantidadQuejas, count(distinct NUMERO_IDENTIFICACION) as cantidadClientes from INFO_PQR where TIPO_PRODUCTO = ? group by descripcionCaso, tipoRadicacion    order by cantidadQuejas desc",
     CLIENT_LIST_CLIENT_BY_COMPLAIN: "SELECT cli.NuMERO_IDENTIFICACiON as identificacion , concat(cli.NOMBRES, cli.APELLIDOS) as nombre, COALESCE(cli.TELEFONO, 'N/A') as telefono, COALESCE(cli.EMAIL, 'N/A') as email FROM mockaru.INFO_CLIENTE cli, INFO_PQR p where p.NUMERO_IDENTIFICACION = cli.NUMERO_IDENTIFICACION and p.DESCRIPCION_CASO = ?",
-    CLIENT_GET_CLIENT_BY_ID: "SELECT concat(NOMBRES, ' ' , APELLIDOS) AS name, COALESCE(EMAIL, 'N/A') AS email, coalesce(TELEFONO, 'N/A') AS phoneNumber FROM mockaru.INFO_CLIENTE WHERE NUMERO_IDENTIFICACION = ?"
+    CLIENT_GET_CLIENT_BY_ID: "SELECT NUMERO_IDENTIFICACION as identificationNumber, concat(NOMBRES, ' ' , APELLIDOS) AS name, COALESCE(EMAIL, 'N/A') AS email, coalesce(TELEFONO, 'N/A') AS phoneNumber FROM mockaru.INFO_CLIENTE WHERE NUMERO_IDENTIFICACION = ?"
     
   };
--- index.js
+++ index.js
@@ -11,20 +11,20 @@ if (cluster.isMaster) {
         cluster.fork();
     }
 
-// Code to run if we're in a worker process
-}else{
+    // Code to run if we're in a worker process
+} else {
     const express = require('express');
-var mysql = require('mysql');
-var cors = require('cors')
-var dashboardRouter = require('./routes/dashboard');
-var bodyParser = require('body-parser');
-const app = express();
-const PORT = 3000;
+    var mysql = require('mysql');
+    var cors = require('cors')
+    var dashboardRouter = require('./routes/dashboard');
+    var bodyParser = require('body-parser');
+    const app = express();
+    const PORT = 3000;
 
 
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/dashboard', dashboardRouter);
-app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
+    app.use(cors());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use('/dashboard', dashboardRouter);
+    app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
 }
